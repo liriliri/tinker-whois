4992ce9943ad75bfec8dc045399258d2f5dff5c3
redhoodsu <redhoodsu@tencent.com>
2026-02-13 15:19:29 +0800

    chore: small changes

--- vite.preload.ts
+++ vite.preload.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import { builtinModules } from 'node:module'
 import path from 'node:path'
 
 const builtins = builtinModules.filter((e) => !e.startsWith('_'))
 builtins.push('electron', ...builtins.map((m) => `node:${m}`))
 
-export default defineConfig(() => {
+export default defineConfig(async (): Promise<UserConfig> => {
   const pkg = require(path.join(process.cwd(), 'package.json'))
 
   return {
