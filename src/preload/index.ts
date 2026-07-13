a3a4a4f622fe0bc36b509fe476cfe96a3b825248
RedHoodSu <surunzi@foxmail.com>
2026-07-13 18:18:38 +0800

    chore: small changes

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,14 +2,14 @@ import { contextBridge } from 'electron'
 import { whoisQuery } from './whoisQuery'
 import type { WhoisResult } from '../common/types'
 
-const whoisObj = {
+const api = {
   query: (resource: string): Promise<WhoisResult> => {
     return whoisQuery(resource)
   },
 }
 
-contextBridge.exposeInMainWorld('whois', whoisObj)
+contextBridge.exposeInMainWorld('whois', api)
 
 declare global {
-  const whois: typeof whoisObj
+  const whois: typeof api
 }
